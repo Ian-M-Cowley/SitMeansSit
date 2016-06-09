5ad5ad0a90eed7f282332fe62541022592bf5d3e
Holly Cowley <Holly@hollys-air.attlocal.net>
2016-06-08 20:22:55 -0500

    Initial commit.

---
+++ app/src/main/java/com/cowley/sitmeanssit/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.cowley.sitmeanssit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ian Cowley on 6/6/16 at 11:37 PM.
+ *
+ * Base Activity to provide functionality to all other Activities.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/cowley/sitmeanssit/home/HomeActivity.java
@@ -0,0 +1,11 @@
+package com.cowley.sitmeanssit.home;
+
+import com.cowley.sitmeanssit.BaseActivity;
+
+/**
+ * Created by Ian Cowley on 6/6/16 at 11:40 PM.
+ */
+
+public class HomeActivity extends BaseActivity {
+
+}
